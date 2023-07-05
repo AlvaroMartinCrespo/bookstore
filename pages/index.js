de8a82bc4a15edee522727184752d686eaac54d1
Alvaro <alvaro.martin.crespo.00@gmail.com>
2023-07-05 17:43:47 +0200

    title

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="">
       <section className="flex flex-col items-center h-screen justify-center">
-        <h1 className="text-3xl font-bold">Social Network</h1>
+        <h1 className="text-3xl font-bold">Bookstore</h1>
         <div className="flex gap-5 items-center mt-5">
           <Link href="/register">Register</Link>
           <Link href="/login">Login</Link>
